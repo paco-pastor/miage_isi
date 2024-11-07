39a70cfbe164ab983403c147f2839c1e29d95783
Paco-Pastor <paco.pastor@hippolyte-rh.fr>
2024-11-07 14:34:20 +0100

    ✅ Finalize tests

--- src/test/java/example/TabAlgosTests.java
+++ src/test/java/example/TabAlgosTests.java
@@ -1,36 +1,64 @@
 package example;
 
-
-import org.main.java.example.TabAlgos; // TODO
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 
-// TODO clasisification des tests (tableau vide, tableau null, tableau non vide, tableaux d'entiers max...)
+
 class TabAlgosTests {
 
+    int[] nullData = null;
+    int[] emptyData = new int[] {};
+    int[] positiveData = new int[] { 1, 2, 3 };
+    int[] negativeData = new int[] { -1, -2, -3 };
+    int[] mixedData = new int[] { -1, 0, 1 };
+    int[] zeroData = new int[] { 0, 0, 0 };
+    int[] maxData = new int[] { Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE };
+    int[] minData = new int[] { Integer.MIN_VALUE, Integer.MIN_VALUE, Integer.MIN_VALUE };
+    int[] maxMinData = new int[] { Integer.MAX_VALUE, Integer.MIN_VALUE };
+    
     @Test
     void plusGrand() {
-        assertEquals(1, TabAlgos.plusGrand(new int[] { 1 }));
-        assertEquals(8, TabAlgos.plusGrand(new int[] { 1, -5, 8, 0 }));
-        assertThrows(IllegalArgumentException.class, () -> TabAlgos.plusGrand(null));
-        assertThrows(IllegalArgumentException.class, () -> TabAlgos.plusGrand(new int[] {}));
+        assertThrows(IllegalArgumentException.class, () -> TabAlgos.plusGrand(nullData));
+        assertThrows(IllegalArgumentException.class, () -> TabAlgos.plusGrand(emptyData));
+        assertEquals(3, TabAlgos.plusGrand(positiveData));
+        assertEquals(-1, TabAlgos.plusGrand(negativeData));
+        assertEquals(1, TabAlgos.plusGrand(mixedData));
+        assertEquals(0, TabAlgos.plusGrand(zeroData));
+        assertEquals(Integer.MAX_VALUE, TabAlgos.plusGrand(maxData));
+        assertEquals(Integer.MIN_VALUE, TabAlgos.plusGrand(minData));
+        assertEquals(Integer.MAX_VALUE, TabAlgos.plusGrand(maxMinData));
     }
 
     @Test
     void moyenne() {
-        assertEquals(2.0, TabAlgos.moyenne(new int[] { 1, 3 }));
-        assertEquals(3.0, TabAlgos.moyenne(new int[] { 1, 3, 5 }));
-        assertThrows(IllegalArgumentException.class, () -> TabAlgos.moyenne(null));
-        assertThrows(IllegalArgumentException.class, () -> TabAlgos.moyenne(new int[] {}));
+        assertThrows(IllegalArgumentException.class, () -> TabAlgos.moyenne(nullData));
+        assertThrows(IllegalArgumentException.class, () -> TabAlgos.moyenne(emptyData));
+        assertEquals(2, TabAlgos.moyenne(positiveData));
+        assertEquals(-2, TabAlgos.moyenne(negativeData));
+        assertEquals(0, TabAlgos.moyenne(mixedData));
+        assertEquals(0, TabAlgos.moyenne(zeroData));
+        assertEquals(Integer.MAX_VALUE, TabAlgos.moyenne(maxData));
+        assertEquals(Integer.MIN_VALUE, TabAlgos.moyenne(minData));
+        assertEquals(-0.5, TabAlgos.moyenne(maxMinData));
     }
 
     @Test
     void egaux() {
-        assertTrue(TabAlgos.egaux(new int[] { 1, 2 }, new int[] { 1, 2 }));
-        assertTrue(TabAlgos.egaux(new int[] { 1, 2, 3 }, new int[] { 1, 2, 3 }));
-        assertFalse(TabAlgos.egaux(new int[] { 1, 2 }, new int[] { 1, 3 }));
-        assertFalse(TabAlgos.egaux(new int[] { 1, 2 }, new int[] { 1, 2, 3 }));
+        assertThrows(NullPointerException.class, () -> TabAlgos.egaux(nullData, nullData));
+        assertTrue(TabAlgos.egaux(emptyData, emptyData));
+        assertTrue(TabAlgos.egaux(positiveData, positiveData));
+        assertTrue(TabAlgos.egaux(negativeData, negativeData));
+        assertTrue(TabAlgos.egaux(mixedData, mixedData));
+        assertTrue(TabAlgos.egaux(zeroData, zeroData));
+        assertTrue(TabAlgos.egaux(maxData, maxData));
+        assertTrue(TabAlgos.egaux(minData, minData));
+        assertTrue(TabAlgos.egaux(maxMinData, maxMinData));
+        assertFalse(TabAlgos.egaux(emptyData, positiveData));
+        assertFalse(TabAlgos.egaux(negativeData, mixedData));
+        assertFalse(TabAlgos.egaux(zeroData, maxData));
+        assertFalse(TabAlgos.egaux(minData, maxMinData));
     }
 
 }
